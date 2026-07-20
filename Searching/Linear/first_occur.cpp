b975472efd5f9b40f5e8e171f3cf087a26077131
janhaviphale18 <janhaviphale0@gmail.com>
2026-07-20 23:55:03 +0530

    Add First Occurrence using Linear Search

---
+++ Searching/Linear/first_occur.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, arr[100], key;
+    bool found = false;
+
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    cout << "Enter the elements in the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter the key to be searched: ";
+    cin >> key;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            cout << "First occurrence found at position " << i + 1;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        cout << "Element not found.";
+    }
+
+    return 0;
+}
