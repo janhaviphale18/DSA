ba08d34a13401bfe918345eb0562caa30932da91
janhaviphale18 <janhaviphale0@gmail.com>
2026-07-20 23:56:11 +0530

    Add Last Occurrence using Linear Search

---
+++ Searching/Linear/last_occur.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, arr[100], key;
+    int position = -1;
+
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    cout << "Enter the elements in the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter the key to be searched: ";
+    cin >> key;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            position = i + 1;
+        }
+    }
+
+    if (position != -1)
+    {
+        cout << "Last occurrence found at position " << position;
+    }
+    else
+    {
+        cout << "Element not found.";
+    }
+
+    return 0;
+}
