03eeea37f62bad6e4819e12f76192b06a8f1c743
janhaviphale18 <janhaviphale0@gmail.com>
2026-07-22 23:33:00 +0530

    Add Position using Linear Search

---
+++ Searching/Linear/position.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, arr[100], key;
+    bool found = false;
+
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    cout << "Enter the elements in the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter the element to search: ";
+    cin >> key;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            cout << "Element found." << endl;
+            cout << "Index    : " << i << endl;
+            cout << "Position : " << i + 1 << endl;
+
+            found = true;
+            break;
+        }
+    }
+
+    if (!found)
+    {
+        cout << "Element not found.";
+    }
+
+    return 0;
+}
