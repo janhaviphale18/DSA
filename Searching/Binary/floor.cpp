5580f423df03d57130e576aeb0f0bec8f41d3e41
janhaviphale18 <janhaviphale0@gmail.com>
2026-07-23 23:45:31 +0530

    Add Floor using Binary Search

---
+++ Searching/Binary/floor.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, arr[100], key;
+    int answer = -1;
+
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    cout << "Enter the sorted elements in the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "Enter the key: ";
+    cin >> key;
+
+    int low = 0;
+    int high = n - 1;
+    int mid;
+
+    while (low <= high)
+    {
+        mid = low + (high - low) / 2;
+
+        if (arr[mid] <= key)
+        {
+            answer = mid;
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    if (answer != -1)
+    {
+        cout << "Floor = " << arr[answer] << endl;
+        cout << "Position = " << answer + 1;
+    }
+    else
+    {
+        cout << "Floor does not exist.";
+    }
+
+    return 0;
+}
