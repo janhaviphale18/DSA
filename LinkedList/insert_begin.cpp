1cd80e89e012e33f03039474c37dc4ecd15c3d71
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-06 20:50:52 +0530

    Add singly linked list insertion at beginning program

---
+++ LinkedList/insert_begin.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+int main()
+{
+    Node* head = NULL;
+    Node* temp = NULL;
+    Node* newNode = NULL;
+
+    int n, value;
+
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        newNode = new Node;
+
+        cout << "Enter data for node " << i << ": ";
+        cin >> newNode->data;
+
+        newNode->next = NULL;
+
+        if(head == NULL)
+        {
+            head = newNode;
+            temp = newNode;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    newNode = new Node;
+
+    cout << "\nEnter the value to insert at the beginning: ";
+    cin >> value;
+
+    newNode->data = value;
+
+    newNode->next = head;
+
+    head = newNode;
+
+    cout << "\nUpdated Linked List: ";
+
+    temp = head;
+
+    while(temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+
+    return 0;
+}
