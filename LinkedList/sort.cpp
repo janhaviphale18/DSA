f202e1e983e64d77796c902048907a3bf6288023
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-08 22:19:17 +0530

    Add singly linked list sorting program

---
+++ LinkedList/sort.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+int main()
+{
+    Node* head = NULL;
+    Node* temp = NULL;
+    Node* newNode = NULL;
+
+    int n;
+
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        newNode = new Node;
+
+        cout << "Enter data for node " << i << ": ";
+        cin >> newNode->data;
+
+        newNode->next = NULL;
+
+        if(head == NULL)
+        {
+            head = newNode;
+            temp = newNode;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    Node* current = head;
+    Node* check = NULL;
+
+    while(current != NULL)
+    {
+        check = current->next;
+
+        while(check != NULL)
+        {
+            if(current->data > check->data)
+            {
+                int value = current->data;
+                current->data = check->data;
+                check->data = value;
+            }
+
+            check = check->next;
+        }
+
+        current = current->next;
+    }
+
+    cout << "\nSorted Linked List: ";
+
+    temp = head;
+
+    while(temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+
+    return 0;
+}
