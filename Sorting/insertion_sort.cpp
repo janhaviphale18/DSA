548037b9fa8f58b2ca6e56824a4690a5d2c09445
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-11 21:53:00 +0530

    Add insertion sort program

---
+++ Sorting/insertion_sort.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int arr[100];
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    cout << "Enter the elements: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    cout << "\nSorted Array: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
