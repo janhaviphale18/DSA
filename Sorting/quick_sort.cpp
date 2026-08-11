ac7ccae6dc58ec1de6b0c5ae1970978b0246280c
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-11 21:55:55 +0530

    Add quick sort program

---
+++ Sorting/quick_sort.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+int partitionArray(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for(int j = low; j < high; j++)
+    {
+        if(arr[j] < pivot)
+        {
+            i++;
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if(low < high)
+    {
+        int pivotIndex = partitionArray(arr, low, high);
+
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+}
+
+int main()
+{
+    int n;
+    int arr[100];
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    cout << "Enter the elements: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "\nSorted Array: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
