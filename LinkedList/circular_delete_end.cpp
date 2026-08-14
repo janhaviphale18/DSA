2c7ba046e179de81c269de3b5e1ad6ab19282e1b
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-14 22:03:09 +0530

    Add circular linked list delete from end program

---
+++ LinkedList/circular_delete_end.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    cout << "Enter the elements: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        int value;
+        cin >> value;
+
+        Node* newNode = new Node;
+        newNode->data = value;
+
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            tail->next = head;
+        }
+        else
+        {
+            newNode->next = head;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    if(head == NULL)
+    {
+        cout << "List is empty." << endl;
+        return 0;
+    }
+
+    if(head == tail)
+    {
+        delete head;
+        head = NULL;
+        tail = NULL;
+    }
+    else
+    {
+        Node* temp = head;
+
+        while(temp->next != tail)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = head;
+
+        delete tail;
+
+        tail = temp;
+    }
+
+    cout << "\nCircular Linked List: ";
+
+    if(head != NULL)
+    {
+        Node* temp = head;
+
+        do
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        while(temp != head);
+    }
+
+    cout << endl;
+
+    return 0;
+}
