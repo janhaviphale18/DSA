4e923360ddef0a8cfe3a59820d27ea7c440862de
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-14 21:47:07 +0530

    Add circular linked list insert at end program

---
+++ LinkedList/circular_insert_end.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    cout << "Enter the elements: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        int value;
+        cin >> value;
+
+        Node* newNode = new Node;
+
+        newNode->data = value;
+
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            tail->next = head;
+        }
+        else
+        {
+            newNode->next = head;
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    int value;
+
+    cout << "Enter the element to insert at end: ";
+    cin >> value;
+
+    Node* newNode = new Node;
+
+    newNode->data = value;
+    newNode->next = head;
+
+    tail->next = newNode;
+    tail = newNode;
+
+    cout << "\nCircular Linked List: ";
+
+    Node* temp = head;
+
+    do
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    while(temp != head);
+
+    cout << endl;
+
+    return 0;
+}
