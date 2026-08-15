bb6afa059bac803180ba4937c08f0684a0a66679
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-15 23:14:43 +0530

    Add circular doubly linked list creation program

---
+++ LinkedList/circular_doubly_create.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* prev;
+    Node* next;
+};
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    cout << "Enter the elements: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        int value;
+        cin >> value;
+
+        Node* newNode = new Node;
+
+        newNode->data = value;
+
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+
+            head->next = head;
+            head->prev = head;
+        }
+        else
+        {
+            newNode->prev = tail;
+            newNode->next = head;
+
+            tail->next = newNode;
+            head->prev = newNode;
+
+            tail = newNode;
+        }
+    }
+
+    cout << "\nForward: ";
+
+    if(head != NULL)
+    {
+        Node* temp = head;
+
+        do
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        while(temp != head);
+    }
+
+    cout << "\nBackward: ";
+
+    if(tail != NULL)
+    {
+        Node* temp = tail;
+
+        do
+        {
+            cout << temp->data << " ";
+            temp = temp->prev;
+        }
+        while(temp != tail);
+    }
+
+    cout << endl;
+
+    return 0;
+}
