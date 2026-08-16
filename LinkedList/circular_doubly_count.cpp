15cfe89b6da4fc0d24c650eb3b54fe82eaf04de6
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-16 22:46:46 +0530

    Add circular doubly linked list count nodes program

---
+++ LinkedList/circular_doubly_count.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* prev;
+    Node* next;
+};
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    cout << "Enter the elements: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        int value;
+        cin >> value;
+
+        Node* newNode = new Node;
+
+        newNode->data = value;
+
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+
+            head->next = head;
+            head->prev = head;
+        }
+        else
+        {
+            newNode->prev = tail;
+            newNode->next = head;
+
+            tail->next = newNode;
+            head->prev = newNode;
+
+            tail = newNode;
+        }
+    }
+
+    int count = 0;
+
+    if(head != NULL)
+    {
+        Node* temp = head;
+
+        do
+        {
+            count++;
+            temp = temp->next;
+        }
+        while(temp != head);
+    }
+
+    cout << "\nNumber of nodes: " << count << endl;
+
+    return 0;
+}
