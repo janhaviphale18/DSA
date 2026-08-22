7911d09c651f5f70c8bf775eceab4061c784d32f
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-22 22:28:31 +0530

    Add next smaller element program

---
+++ Stack/next_smaller.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int value)
+{
+    top++;
+    stack[top] = value;
+}
+
+int pop()
+{
+    int value = stack[top];
+    top--;
+
+    return value;
+}
+
+int peek()
+{
+    return stack[top];
+}
+
+int main()
+{
+    int n;
+    int arr[MAX];
+    int result[MAX];
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int i = n - 1; i >= 0; i--)
+    {
+        while(top != -1 && peek() >= arr[i])
+        {
+            pop();
+        }
+
+        if(top == -1)
+        {
+            result[i] = -1;
+        }
+        else
+        {
+            result[i] = peek();
+        }
+
+        push(arr[i]);
+    }
+
+    cout << "Next Smaller Elements: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << result[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
