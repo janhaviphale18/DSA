99d383babe6f52613fb2373bf103cbc47166bb0a
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-23 18:59:17 +0530

    Added basic queue program

---
+++ Queue/queue.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 5
+
+int queueArr[MAX];
+int front = -1;
+int rear = -1;
+
+void enqueue(int value)
+{
+    if (rear == MAX - 1)
+    {
+        cout << "Queue Overflow" << endl;
+        return;
+    }
+
+    if (front == -1)
+    {
+        front = 0;
+    }
+
+    rear++;
+    queueArr[rear] = value;
+
+    cout << value << " inserted into queue" << endl;
+}
+
+void dequeue()
+{
+    if (front == -1 || front > rear)
+    {
+        cout << "Queue Underflow" << endl;
+        return;
+    }
+
+    cout << queueArr[front] << " deleted from queue" << endl;
+    front++;
+}
+
+void display()
+{
+    if (front == -1 || front > rear)
+    {
+        cout << "Queue is empty" << endl;
+        return;
+    }
+
+    cout << "Queue: ";
+
+    for (int i = front; i <= rear; i++)
+    {
+        cout << queueArr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    display();
+
+    dequeue();
+
+    display();
+
+    enqueue(40);
+    enqueue(50);
+
+    display();
+
+    return 0;
+}
