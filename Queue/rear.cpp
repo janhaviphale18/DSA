4254fcefe5af12100e4712ad53187e2a8ff063ff
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-26 12:28:07 +0530

    Added rear operation program

---
+++ Queue/rear.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 5
+
+int queueArr[MAX];
+int front = -1;
+int rear = -1;
+
+void enqueue(int value)
+{
+    if (rear == MAX - 1)
+    {
+        cout << "Queue Overflow" << endl;
+        return;
+    }
+
+    if (front == -1)
+    {
+        front = 0;
+    }
+
+    rear++;
+    queueArr[rear] = value;
+
+    cout << value << " inserted into queue" << endl;
+}
+
+void rearElement()
+{
+    if (front == -1 || front > rear)
+    {
+        cout << "Queue Underflow" << endl;
+        return;
+    }
+
+    cout << "Rear element: " << queueArr[rear] << endl;
+}
+
+void display()
+{
+    if (front == -1 || front > rear)
+    {
+        cout << "Queue is empty" << endl;
+        return;
+    }
+
+    cout << "Queue: ";
+
+    for (int i = front; i <= rear; i++)
+    {
+        cout << queueArr[i] << " ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    rearElement();
+
+    display();
+
+    return 0;
+}
