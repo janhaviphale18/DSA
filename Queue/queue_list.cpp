b2f1e07367c2116aff47abdafe4f2b57966cc3c0
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-26 12:39:13 +0530

    Added queue using linked list program

---
+++ Queue/queue_list.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+Node* front = NULL;
+Node* rear = NULL;
+
+void enqueue(int value)
+{
+    Node* newNode = new Node();
+
+    newNode->data = value;
+    newNode->next = NULL;
+
+    if (rear == NULL)
+    {
+        front = rear = newNode;
+    }
+    else
+    {
+        rear->next = newNode;
+        rear = newNode;
+    }
+
+    cout << value << " inserted into queue" << endl;
+}
+
+void dequeue()
+{
+    if (front == NULL)
+    {
+        cout << "Queue Underflow" << endl;
+        return;
+    }
+
+    Node* temp = front;
+
+    cout << front->data << " deleted from queue" << endl;
+
+    front = front->next;
+
+    if (front == NULL)
+    {
+        rear = NULL;
+    }
+
+    delete temp;
+}
+
+void display()
+{
+    if (front == NULL)
+    {
+        cout << "Queue is empty" << endl;
+        return;
+    }
+
+    Node* temp = front;
+
+    cout << "Queue: ";
+
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+
+    dequeue();
+
+    display();
+
+    return 0;
+}
