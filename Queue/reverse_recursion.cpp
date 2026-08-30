71c1c08e6b4f0e28c63e866fdfd66f6db4a73ac1
janhaviphale18 <janhaviphale0@gmail.com>
2026-08-30 21:07:00 +0530

    Added queue reversal using recursion program

---
+++ Queue/reverse_recursion.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void reverseQueue(queue<int>& q)
+{
+    if (q.empty())
+    {
+        return;
+    }
+
+    int value = q.front();
+    q.pop();
+
+    reverseQueue(q);
+
+    q.push(value);
+}
+
+void display(queue<int> q)
+{
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    queue<int> q;
+
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+
+    cout << "Original Queue: ";
+    display(q);
+
+    reverseQueue(q);
+
+    cout << "Reversed Queue: ";
+    display(q);
+
+    return 0;
+}
